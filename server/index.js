4ff9a0b11e3887ef5b742e0a476d6551437f5608
Ryan Tang <ryan.tang.clu@gmail.com>
2023-01-04 11:52:41 -0500

    first commit - server setup

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, console.log(`Server is running on port ${port}.`));
